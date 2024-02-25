a1e7dd6310e0ebf8733bbe6946eaad71b9198509
Stas <stasmotorny@gmail.com>
2024-02-25 14:47:20 +0200

    Firebase auth is setted but not tested. Added test for navigation.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   preset: 'react-native',
-  setupFilesAfterEnv: ['@testing-library/react-native/extend-expect'],
+  setupFilesAfterEnv: ['@testing-library/react-native/extend-expect', './jest-setup.ts'],
+  transformIgnorePatterns: ["/node_modules/(?!@firebase)/"],
 };
