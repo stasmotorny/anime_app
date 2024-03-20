4815fbc872c69bedfb0d386f129a610f2f7493bf
Stas <stasmotorny@gmail.com>
2024-03-20 23:17:25 +0200

    Reactive var initial value was changed to satisfy tests conditions.

--- src/reactiveVariablesStore/choosenSortType.ts
+++ src/reactiveVariablesStore/choosenSortType.ts
@@ -1,4 +1,4 @@
 import {makeVar} from '@apollo/client';
 import {MediaSort} from '../API/__generated__/graphql.ts';
 
-export const chosenSortType = makeVar<MediaSort>(MediaSort.Score);
+export const chosenSortType = makeVar<MediaSort>(MediaSort.PopularityDesc);
